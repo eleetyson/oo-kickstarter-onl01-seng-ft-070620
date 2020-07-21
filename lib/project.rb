b6bbfac828cb3e56fd82c354328dd0e51eedd606
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 13:00:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Project
   # attr_accessor
-  attr_reader :title
+  attr_reader :title, :backers
   
   def initialize(title)
     @title = title
-    
+    @backers = []
   end
   
   
