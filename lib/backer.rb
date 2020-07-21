c050bc310358113a21d920254206e31bdf2d71ce
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 13:00:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,10 +3,13 @@ class Backer
   # attr_accessor
   attr_reader :name, :backed_projects
   
-  
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
+
+  def back_project
+    
   end
 
 end
