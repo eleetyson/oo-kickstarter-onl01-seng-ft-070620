d867f979bf54af9a9ee415f421c19120f0d42151
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 13:05:23 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   def back_project(project)
     @backed_projects << project
     # add project instance to the Project class' 
-    project.add_backer()
+    project.add_backer(self)
   end
 
 end
